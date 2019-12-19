f613b0cdf56d3f33df19ef87bee053864f639590
Joel Miller <miller345@me.com>
2019-12-19 18:45:10 +0000

    day 6 pt1

---
+++ 6/index.ts
@@ -0,0 +1,26 @@
+import { readFileStr } from "https://deno.land/std/fs/read_file_str.ts";
+
+const rawInput: string = await readFileStr("./6/input.txt");
+
+interface Orb {
+  id: string;
+  orbits: string;
+}
+
+let input = rawInput.split("\n").reduce((obj, line) => {
+  let x = line.split(")");
+  return {
+    ...obj,
+    [x[1]]: { id: x[1], orbits: x[0] }
+  };
+}, {}) as { [id: string]: Orb };
+
+let bodies = Object.keys(input);
+
+let ancestors = (id, arr = []) => {
+  let orbits = input[id].orbits;
+  arr = [...arr, orbits];
+  return orbits === "COM" ? arr : ancestors(orbits, arr);
+};
+
+console.log("Part 1:", bodies.map(b => ancestors(b)).flat().length);
