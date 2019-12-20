4a522b276688205131ac917a4758f67703c199e4
Joel Miller <miller345@me.com>
2019-12-20 22:35:15 +0000

    day 8 pt 2

--- 8/index.ts
+++ 8/index.ts
@@ -2,11 +2,14 @@ import { readFileStr } from "https://deno.land/std/fs/read_file_str.ts";
 
 const rawInput: string = await readFileStr("./8/input.txt");
 
-let getLayerNumber = (index: number, width: number, height: number) =>
+const WIDTH = 25;
+const HEIGHT = 6;
+
+let getLayerNumber = (index: number, width = WIDTH, height = HEIGHT) =>
   Math.floor(index / (width * height));
 
 let data = rawInput.split("").reduce((arr, s, i) => {
-  let layer = getLayerNumber(i, 25, 6);
+  let layer = getLayerNumber(i);
   let counts = arr[layer] ? arr[layer] : {};
   arr[layer] = { ...counts, [s]: counts[s] ? counts[s] + 1 : 1 };
   return arr;
@@ -21,3 +24,20 @@ let layer = data.reduce((obj, counts, layer) => {
 }, null).layer;
 
 console.log("Part 1:", data[layer][1] * data[layer][2]);
+
+let img = rawInput
+  .split("")
+  .reduce((arr, s, i) => {
+    let layer = getLayerNumber(i);
+    let pixelIndex = i - WIDTH * HEIGHT * layer;
+    if (arr[pixelIndex] == null || arr[pixelIndex] === "2") {
+      arr[pixelIndex] = s;
+    }
+    return arr;
+  }, [])
+  .map(x => (x == "1" ? "X" : " "));
+
+console.log("Part 2:");
+for (let i = 0; i < HEIGHT; i++) {
+  console.log(img.splice(0, WIDTH).join(""));
+}
